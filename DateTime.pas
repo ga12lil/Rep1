5e216b52f3106c9f6cd197637c077603a9039290
Artem Grigorenko <agrigoren@sfedu.ru>
2021-10-21 08:51:17 +0300

    Add func IsLeapYear

---
+++ DateTime.pas
@@ -0,0 +1,13 @@
+﻿function IsLeapYear(year: integer):boolean;
+begin
+  if (year mod 4 = 0) and (year mod 100 <> 0) then
+    Result:=true
+  else if year mod 400 = 0 then
+    Result:=true
+  else
+    Result:=false;
+end;
+
+begin
+  //print(IsLeapYear(257));
+end.
